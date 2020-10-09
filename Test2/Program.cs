861bf4074d54600b2941bf4e95aa1f61cabc333d
kilian vi <vi.kilian@live.fr>
2020-10-09 13:21:17 +0200

    cw "hello world"

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test2
+namespace Github
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello world");
         }
     }
 }
